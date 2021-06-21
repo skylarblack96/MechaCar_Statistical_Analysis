9593b06a318427df1d51e4c177186dfe9671b1ab
skylarblack96 <79720084+skylarblack96@users.noreply.github.com>
2021-06-20 21:38:23 -0500

    Final Project Complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,11 +2,11 @@ library(dplyr)
 
 mecha_car <- read.csv('MechaCar_mpg.csv')
 
-lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + 
-     AWD + mpg, data = mecha_car)
+lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + 
+     AWD + vehicle_length, data = mecha_car)
 
-summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + 
-             AWD + mpg, data = mecha_car))
+summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + 
+             AWD + vehicle_length, data = mecha_car))
 
 
 suspension_coil <- read.csv('Suspension_Coil.csv')
@@ -21,15 +21,15 @@ lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>%
   summarize(Mean = mean(PSI),Median = median(PSI),Variance = var(PSI),
   SD = sd(PSI),.groups = 'keep')
 
-t.test(log10(suspension_coil$PSI), mu=1500)
+t.test(suspension_coil$PSI, mu=1500)
 
 Lot1_test <- subset(suspension_coil, Manufacturing_Lot == 'Lot1')
-t.test(log10(Lot1_test$PSI), mu=1500)
+t.test(Lot1_test$PSI, mu=1500)
 
 Lot2_test <- subset(suspension_coil, Manufacturing_Lot == 'Lot2')
-t.test(log10(Lot2_test$PSI), mu=1500)
+t.test(Lot2_test$PSI, mu=1500)
 
 Lot3_test <- subset(suspension_coil, Manufacturing_Lot == 'Lot3')
-t.test(log10(Lot3_test$PSI), mu=1500)
+t.test(Lot3_test$PSI, mu=1500)
 
 
